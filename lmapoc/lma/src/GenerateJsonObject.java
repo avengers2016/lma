7094589c72d0ac955171ea088135a663fe7bf167
Rahul <avengers2016.lma@.com>
2016-06-05 04:08:34 +0530

    Json generation

--- lmapoc/lma/src/GenerateJsonObject.java
+++ lmapoc/lma/src/GenerateJsonObject.java
@@ -1,10 +1,14 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 
 import com.lma.model.CustomerContract;
+import com.lma.model.CustomerTransactionsDetails;
+import com.lma.model.PersonalInfo;
 
 
 public class GenerateJsonObject {
@@ -13,7 +17,20 @@ public class GenerateJsonObject {
 		
 		ObjectMapper mapper = new ObjectMapper();
 		CustomerContract CustomerContract =  new  CustomerContract();
+		List<CustomerTransactionsDetails>  customerTransactionsDetails =  new ArrayList<CustomerTransactionsDetails>();
 		
+		CustomerTransactionsDetails CustomerTransactionsDetails1 = new  CustomerTransactionsDetails();
+		CustomerTransactionsDetails CustomerTransactionsDetails2 = new  CustomerTransactionsDetails();
+		
+		PersonalInfo personalInfo1 = new PersonalInfo();
+		PersonalInfo personalInfo2 = new PersonalInfo();
+		
+		CustomerTransactionsDetails1.setPersonalInfo(personalInfo1);
+		CustomerTransactionsDetails2.setPersonalInfo(personalInfo2);
+		customerTransactionsDetails.add(CustomerTransactionsDetails1);
+		customerTransactionsDetails.add(CustomerTransactionsDetails2);
+		
+		CustomerContract.setCustomerTransactionsDetails(customerTransactionsDetails);
 		
 		
 		try {
